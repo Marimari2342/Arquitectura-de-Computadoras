19dea62d5655be56c897895a1bde443ae72fde79
Marimari2342 <126342343+Marimari2342@users.noreply.github.com>
2023-09-09 20:13:00 -0300

    Practica2-Punto3.asm

--- Practica2-Punto3.asm
+++ Practica2-Punto3.asm
@@ -1,6 +1,6 @@
-3) * Escribir un programa que muestre en pantalla las letras del abecedario, sin espacios, intercalando mayúsculas y
-minúsculas (AaBb…), sin incluir texto en la memoria de datos del programa. Tener en cuenta que el código de “A” es 41H,
-el de “a” es 61H y que el resto de los códigos son correlativos según el abecedario.
+;3) * Escribir un programa que muestre en pantalla las letras del abecedario, sin espacios, intercalando mayúsculas y
+;minúsculas (AaBb…), sin incluir texto en la memoria de datos del programa. Tener en cuenta que el código de “A” es 41H,
+;el de “a” es 61H y que el resto de los códigos son correlativos según el abecedario.
 
 ORG 1000H
 PRI_MAY DB 'A'
