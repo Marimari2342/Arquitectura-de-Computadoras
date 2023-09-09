ecd308524940a9338454d4fe2547c6d749c10f04
Marimari2342 <126342343+Marimari2342@users.noreply.github.com>
2023-09-09 20:12:22 -0300

    Practica2-Punto2.asm

--- Practica2-Punto2.asm
+++ Practica2-Punto2.asm
@@ -1,10 +1,9 @@
-2) Escribir un programa que muestre en pantalla todos los caracteres disponibles en el simulador MSX88, comenzando con
-el caracter cuyo código es el número 01H.
+;2) Escribir un programa que muestre en pantalla todos los caracteres disponibles en el simulador 
+;MSX88, comenzando con el caracter cuyo código es el número 01H.
 
 ORG 1000H
 INI DB 01H
 FIN DB FFH
-
 
 ORG 2000H
 MOV AL,FIN
